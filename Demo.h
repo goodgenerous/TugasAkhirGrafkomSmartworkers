8366983e62b5c4cd8f0e860884833f5749bd771f
Bagus Dermawan <72683434+goodgenerous@users.noreply.github.com>
2022-10-28 11:06:25 +0700

    Add files via upload

---
+++ Demo.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	float angle = 0;
+	float position = -1;
+	float position2 = -2;
+	float initpos = 0;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow* window);
+	void BuildColoredPlane();
+	void BuildRoad();
+	void DrawColoredPlane();
+	void DrawRoad();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+
+};
